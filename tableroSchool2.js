2a128fa05787dc366de599d76683198007110250
Joaco-r <rave111301@gmail.com>
2024-09-01 13:12:08 -0300

    Update tableroSchool2.js

--- tableroSchool2.js
+++ tableroSchool2.js
@@ -2,10 +2,7 @@
 
     const board = document.querySelector('#board-play-computer');
     board.style.backgroundImage = "url('https://assets-themes.chess.com/image/jz5it/200.jpg')";
-
-    const king = document.querySelector('#board-play-computer .piece.wk');
-    king.style.backgroundImage = "url('https://assets-themes.chess.com/image/uf7ut/150/wk.png')";
-
+    
     const plantillaDeEstilo = document.getElementById('board-styles-play-computer');
     fetch('https://joaco-r.github.io/chess-bookmarklet/tableroSchool.html')
     .then(response => response.text())
