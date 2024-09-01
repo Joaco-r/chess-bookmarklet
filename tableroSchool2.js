17a9f24966162e1eca421d3bb3f1479a0a850ab6
Joaco-r <rave111301@gmail.com>
2024-09-01 01:34:37 -0300

    Update tableroSchool2.js

--- tableroSchool2.js
+++ tableroSchool2.js
@@ -1,5 +1,5 @@
 (function(){
     const plantillaDeEstilo = document.getElementById('board-styles-play-computer');
-    plantillaDeEstilo.innerHTML = fetch(' "https://joaco-r.github.io/chess-bookmarklet/tableroSchool.html').text();
+    plantillaDeEstilo.innerHTML = fetch('https://joaco-r.github.io/chess-bookmarklet/tableroSchool.html').text();
     
 })();
