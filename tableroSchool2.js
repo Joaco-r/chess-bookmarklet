e17a2bd1d5beed269627ed392a7d46ffde22060b
Joaco-r <rave111301@gmail.com>
2024-09-01 13:04:39 -0300

    Update tableroSchool2.js

--- tableroSchool2.js
+++ tableroSchool2.js
@@ -1,6 +1,11 @@
 (function(){
-    let board = document.querySelector('#board-play-computer');
+
+    const board = document.querySelector('#board-play-computer');
     board.style.backgroundImage = "url('https://assets-themes.chess.com/image/jz5it/200.jpg')";
+
+    const king = document.getElementById();
+    king.style.backgroundImage = "url('https://assets-themes.chess.com/image/uf7ut/150/wk.png')";
+
     const plantillaDeEstilo = document.getElementById('board-styles-play-computer');
     fetch('https://joaco-r.github.io/chess-bookmarklet/tableroSchool.html')
     .then(response => response.text())
